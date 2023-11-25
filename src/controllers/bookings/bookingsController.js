97aca7080a9e08a3064872d808c444f8577439af
Shihab Hasan <shihabbd018@gmail.com>
2023-11-26 01:26:40 +0600

    addded api to get user specific bookings

--- src/controllers/bookings/bookingsController.js
+++ src/controllers/bookings/bookingsController.js
@@ -1,5 +1,29 @@
 const bookingCollection = require("../../models/bookings/bookingsModel");
 
+
+
+
+//get user specific bookings
+const getUserSpecificBookings = async (req, res, next) => {
+    try {
+        const { email } = req.query;
+        if (req.email.email !== email) {
+            return res.status(403).send({ message: "forbidden" });
+        }
+        const query = { email: email };
+        const bookings = await bookingCollection.find(query);
+        res.send(bookings);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+
+
+
+
+
 //post a new booking
 const postBooking = async (req, res, next) => {
     try {
@@ -14,4 +38,4 @@ const postBooking = async (req, res, next) => {
 }
 
 
-module.exports = { postBooking };
+module.exports = { postBooking, getUserSpecificBookings };
--- src/routes/bookings/bookingsRoutes.js
+++ src/routes/bookings/bookingsRoutes.js
@@ -1,9 +1,13 @@
-const { postBooking } = require("../../controllers/bookings/bookingsController");
+const { postBooking, getUserSpecificBookings } = require("../../controllers/bookings/bookingsController");
 const verifyToken = require("../../middlewares/verifyToken");
 
 const router = require("express").Router();
 
 
+//Get user specific bookings
+router.get("/booking", verifyToken, getUserSpecificBookings)
+
+//Post a new booking
 router.post("/bookings", verifyToken, postBooking);
 
 module.exports = router;
